4d9f001bc069b70a4d6f795d5edf77d6ed9ba3c7
Simone-Corso <scorso26@gmail.com>
2023-11-19 17:14:19 +0100

    ho creato un array della lista della spesa

--- lista.js
+++ lista.js
@@ -1,3 +1,7 @@
 //Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) 
 //gli elementi della lista individualmente con un ciclo while.
 
+//Creo una array per la lista della spesa : 
+
+const listaSpesa = ['sugo','carcioffi','sale','melenzane','cocola'];
+
